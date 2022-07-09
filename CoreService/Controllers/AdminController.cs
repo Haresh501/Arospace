81ba4f6a5035621ee84bf9a0ebdf30c40580939f
Haresh501 <35988121+Haresh501@users.noreply.github.com>
2022-07-09 18:19:36 +0530

    check function app

--- CoreService/Controllers/AdminController.cs
+++ CoreService/Controllers/AdminController.cs
@@ -7,8 +7,6 @@ using CoreService.Repository;
 using Microsoft.AspNetCore.Mvc;
 using CoreService.Models;
 
-
-
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CoreService.Controllers
